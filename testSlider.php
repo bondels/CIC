3f107da172949742597672405eb47c16eb77bdb4
bondels <gilmerleon@gmail.com>
2017-09-22 14:42:10 -0300

    Add files via upload

---
+++ testSlider.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+<link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="css/agency.css" rel="stylesheet">
+</head>
+<body>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1" class="icono1 "></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+	      <img class="d-block img-fluid" src="img/bannerPremium.jpg" alt="First slide">
+	      <span>Espuma altamente respirable que disipa el calor, de tacto y sensación hiper suave, la cual posee una alta capacidad de adaptación al cuerpo, además de una gran durabilidad. Esta espuma garantiza que mantendrá un excelente rendimiento incluso después de un uso intensivo.</span>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block img-fluid" src="img/algodon.jpg" alt="Second slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block img-fluid" src="img/hiperSoft2.jpg" alt="Third slide">
+	    </div>
+	     <div class="carousel-item">
+	      <img class="d-block img-fluid" src="img/pocket2.jpg" alt="four slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block img-fluid" src="img/lumbar2.jpg" alt="Five slide">
+	    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</body>
+
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/popper/popper.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!-- Plugin JavaScript -->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+<!-- Contact form JavaScript -->
+<script src="js/jqBootstrapValidation.js"></script>
+<script src="js/contact_me.js"></script>
+<!-- Custom scripts for this template -->
+<script src="js/agency.min.js"></script>
+<script>
+$('.carousel').carousel({
+	  interval: false
+	})
+</script>
+</html>
